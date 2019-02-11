6a91eb401b759ea15ef9716242f5ab7245c839cf
Daniil Polykovskiy <daniil.polykovskiy@gmail.com>
2019-02-11 13:23:30 +0300

    import calculate_frechet_distance (#3)

--- fcd_torch/__init__.py
+++ fcd_torch/__init__.py
@@ -1,3 +1,4 @@
 from .fcd import FCD
+from .fcd import calculate_frechet_distance
 
-__all__ = ['FCD']
+__all__ = ['FCD', 'calculate_frechet_distance']
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='utf8') as fh:
     long_description = fh.read()
 
 setup(name='fcd_torch',
-      version='1.0.4',
+      version='1.0.5',
       author='',
       author_email='',
       description='Fréchet ChemNet Distance on PyTorch',
